eb9f38aeaa31954b822677fe64f8d96a3e48b268
Julien MacBook Pro <julien.f2004@icloud.com>
2025-11-29 18:15:41 +0100

    first commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {};
+
+module.exports = {
+  allowedDevOrigins: ["localhost", "10.0.0.*"],
+};
+
+export default nextConfig;
